0501e982207cfd26ef66e51829f90b843ef33f33
Nboxff <211250137@smail.nju.edu.cn>
2023-07-01 20:09:39 +0800

    feat: add module, bulider and visitBlock method in IRGenVisitor.java

--- src/main/java/IRGenVisitor.java
+++ src/main/java/IRGenVisitor.java
@@ -1,6 +1,5 @@
-import IRBuider.Register;
-import Scope.GlobalScope;
-import Scope.Scope;
+import IRBuider.*;
+import Scope.*;
 import Type.Type;
 import antlr.SysYParser;
 import antlr.SysYParserBaseVisitor;
@@ -10,22 +9,37 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.RuleNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
+import static IRBuider.IRBuilder.IRCreateBuilder;
+import static IRBuider.IRModule.IRModuleCreateWithName;
 import static Type.FloatType.IRFloatType;
 import static Type.Int32Type.IRInt32Type;
 
-public class IRGenVisitor extends SysYParserBaseVisitor<Register> {
-    Type int32Type = IRInt32Type();
-    Type floatType = IRFloatType();
+public class IRGenVisitor extends SysYParserBaseVisitor<ValueRef> {
+    IRModule module = IRModuleCreateWithName("module");
+    IRBuilder builder = IRCreateBuilder();
+    private static final Type int32Type = IRInt32Type();
+    private static final Type floatType = IRFloatType();
     private GlobalScope globalScope = null;
     private Scope currentScope = null;
     private int localScopeCounter = 0;
 
+    public IRModule getModule() {
+        return module;
+    }
     @Override
-    public Register visitProgram(SysYParser.ProgramContext ctx) {
+    public ValueRef visitProgram(SysYParser.ProgramContext ctx) {
         globalScope = new GlobalScope("globalScope", null);
         currentScope = globalScope;
         return super.visitProgram(ctx);
     }
 
-
+    @Override
+    public ValueRef visitBlock(SysYParser.BlockContext ctx) {
+        String scopeName = "localScope" + localScopeCounter;
+        localScopeCounter++;
+        currentScope = new LocalScope(scopeName, currentScope);
+        ValueRef ret = super.visitBlock(ctx);
+        currentScope = currentScope.getEnclosingScope();
+        return ret;
+    }
 }
