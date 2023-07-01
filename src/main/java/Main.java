7c23b610a8f53bffd8b59d7ffdbcfa83e7c4b66c
Nboxff <211250137@smail.nju.edu.cn>
2023-07-01 09:35:11 +0800

    feat: add call lexer, parser, irGen in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,28 @@
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        if (args.length < 2) {
+            System.out.println("input path is required");
+        }
+        String source = args[0];
+        String dest = args[1];
+
+        CharStream input = CharStreams.fromFileName(source);
+        // Lexer
+        SysYLexer sysYLexer = new SysYLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(sysYLexer);
+        // Parser
+        SysYParser sysYParser = new SysYParser(tokens);
+        ParseTree tree = sysYParser.program();
+        // Generate intermediate code(IR)
+        IRGenVisitor irGenVisitorVisitor = new IRGenVisitor();
+        irGenVisitorVisitor.visit(tree);
 
     }
 }
