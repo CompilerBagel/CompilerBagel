7418f378cf4ccb2521167ca0473390a30031b352
huangwei021230 <1138608506@qq.com>
2023-07-06 23:50:40 +0800

    feat: GEPInstruction

---
+++ src/main/java/instruction/GetElemPtrInstruction.java
@@ -0,0 +1,50 @@
+package instruction;
+
+import IRBuilder.BaseBlock;
+import IRBuilder.ValueRef;
+import Type.Type;
+
+import java.util.ArrayList;
+import java.util.List;
+import Type.PointerType;
+import Type.ArrayType;
+import static IRBuilder.IRConstants.GETPTR;
+import static Type.Int32Type.IRInt32Type;
+
+public class GetElemPtrInstruction extends Instruction{
+    private ValueRef base;
+    private ArrayList<ValueRef> indexs;
+
+    public GetElemPtrInstruction(GetElemPtrInstruction rhs) {
+        super(rhs);
+        this.base = rhs.base;
+        this.indexs = (ArrayList<ValueRef>) rhs.indexs.clone();
+    }
+
+    public GetElemPtrInstruction(List<ValueRef> operands, BaseBlock basicBlock) {
+        super(operands, basicBlock);
+        base = operands.get(1);
+        for(int i = 2;i<operands.size();i++){
+            indexs.add(operands.get(i));
+        }
+    }
+
+    @Override
+    public String toString() {
+        ValueRef resRegister = operands.get(0);
+        Type baseType = ((PointerType) base.getType()).getBaseType();
+        Type resType;
+        if (baseType instanceof ArrayType) {
+            resType = ((ArrayType) baseType).getElementType();
+        } else {
+            resType = baseType;
+        }
+        StringBuilder indexStrBuilder = new StringBuilder();
+        for (ValueRef index : indexs) {
+            indexStrBuilder.append(", ").append(IRInt32Type().getText())
+                    .append(" ").append(index.getText());
+        }
+        return resRegister.getText() + " = " + GETPTR + " " + baseType.getText() + ", "
+                + base.getTypeText() + " " + base.getText() + indexStrBuilder.toString();
+    }
+}
--- src/main/java/instruction/StoreInstruction.java
+++ src/main/java/instruction/StoreInstruction.java
@@ -1,2 +1,27 @@
-package instruction;public class StoreInstruction {
+package instruction;
+
+import IRBuilder.BaseBlock;
+import IRBuilder.ValueRef;
+
+import java.util.List;
+
+public class StoreInstruction extends Instruction{
+    private ValueRef ptr;
+    private ValueRef target;
+    private ValueRef actualPtr;
+    public StoreInstruction(StoreInstruction rhs){
+        super(rhs);
+    }
+    public StoreInstruction(List<ValueRef> operands, BaseBlock basicBlock) {
+        super(operands, basicBlock);
+        this.target = operands.get(1);
+        this.ptr = operands.get(2);
+    }
+
+    @Override
+    public String toString() {
+        return "store " + target.getType() +  " " +
+                target.getText() + ", " + ptr.getType()
+                + " " + ptr.getText();
+    }
 }
