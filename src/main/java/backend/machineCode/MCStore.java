1b89f232939ea3c7815f320e00d0d98d24fd0b9b
cardigan1008 <211250058@smail.nju.edu.cn>
2023-07-13 16:55:06 +0800

    feat: store

--- src/main/java/backend/machineCode/MCStore.java
+++ src/main/java/backend/machineCode/MCStore.java
@@ -2,20 +2,24 @@ package backend.machineCode;
 
 import java.util.ArrayList;
 
-public class MCMove extends MachineCode {
+public class MCStore extends MachineCode {
     private MachineOperand src;
+    private MachineOperand offset = new MachineOperand(0);
     private MachineOperand dest;
     
+    public void setOffset(final MachineOperand offset) {
+        this.offset = offset;
+    }
+    
     /**
-     * Move.
-     * instruction: mv rd, rs1
-     * exec: reg[rd] <= reg[rs1] + 0
+     * Store Word.
+     * instruction: sw rs2, offset(rs1)
+     * exec: mem[reg[rs1] + offset] <= reg[rs2]
      * @return str
      */
     @Override
     public String toString() {
-        // TODO: mv conditions
-        return "mv " + dest.toString() + ", " + src.toString();
+        return "sw " + src.toString() + ", " + offset.toString() + "(" + dest.toString() + ")";
     }
     
     @Override
@@ -23,19 +27,26 @@ public class MCMove extends MachineCode {
         super.initUseAndDef();
         use.add(src);
         def.add(dest);
-    }
-    
-    @Override
-    public void replaceDef(final MachineOperand oldOperand, final MachineOperand newOperand) {
-        if (oldOperand.equals(this.src)) {
-            this.src = newOperand;
+        if (!offset.isImm()) {
+            use.add(offset);
         }
     }
     
     @Override
+    public void replaceDef(final MachineOperand oldOperand, final MachineOperand newOperand) {
+    
+    }
+    
+    @Override
     public void replaceUse(final MachineOperand oldOperand, final MachineOperand newOperand) {
+        if (oldOperand.equals(this.src)) {
+            this.src = newOperand;
+        }
         if (oldOperand.equals(this.dest)) {
             this.dest = newOperand;
+        }
+        if (oldOperand.equals(this.offset)) {
+            this.offset = newOperand;
         }
     }
     
@@ -47,6 +58,9 @@ public class MCMove extends MachineCode {
         }
         if (this.dest.isPhysicsReg()) {
             phyRegs.add(this.dest);
+        }
+        if (this.offset.isPhysicsReg()) {
+            phyRegs.add(this.offset);
         }
         return phyRegs;
     }
