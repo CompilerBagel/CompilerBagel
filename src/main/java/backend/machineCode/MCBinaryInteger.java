b262a8c3be7da1ff1c37334cc5b7df3ac02f18ce
cardigan1008 <211250058@smail.nju.edu.cn>
2023-07-13 17:30:50 +0800

    feat(MCBinaryInteger): MCBinaryInteger

---
+++ src/main/java/backend/machineCode/MCBinaryInteger.java
@@ -0,0 +1,78 @@
+package backend.machineCode;
+
+import IRBuilder.IRConstants;
+
+import java.util.ArrayList;
+
+public class MCBinaryInteger extends MachineCode {
+    private MachineOperand dest;
+    private MachineOperand left;
+    private MachineOperand right;
+    private String binaryOp;
+    
+    /**
+     * add, sub, mul, div
+     * @return str
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        switch (binaryOp) {
+            case IRConstants.ADD:  // add rd, rs1, rs2
+                sb.append("add ");
+                break;
+            case IRConstants.SUB:  // sub rd, rs1, rs2
+                sb.append("sub ");
+                break;
+            case IRConstants.MUL:  // mul rd, rs1, rs2
+                sb.append("mul ");
+                break;
+            case IRConstants.DIV:  // div rd, rs1, rs2
+                sb.append("div ");
+                break;
+        }
+        
+        sb.append(dest.toString()).append(", ").append(left.toString()).append(", ").append(right.toString());
+        return sb.toString();
+    }
+    
+    @Override
+    public void initUseAndDef() {
+        super.initUseAndDef();
+        def.add(dest);
+        use.add(left);
+        use.add(right);
+    }
+    
+    @Override
+    public void replaceDef(final MachineOperand oldOperand, final MachineOperand newOperand) {
+        if (dest.equals(oldOperand)) {
+            dest = newOperand;
+        }
+    }
+    
+    @Override
+    public void replaceUse(final MachineOperand oldOperand, final MachineOperand newOperand) {
+        if (left.equals(oldOperand)) {
+            left = newOperand;
+        }
+        if (right.equals(oldOperand)) {
+            right = newOperand;
+        }
+    }
+    
+    @Override
+    public ArrayList<MachineOperand> allocatePhyRegs() {
+        ArrayList<MachineOperand> phyRegs = new ArrayList<>();
+        if (dest.isPhysicsReg()) {
+            phyRegs.add(dest);
+        }
+        if (left.isPhysicsReg()) {
+            phyRegs.add(left);
+        }
+        if (right.isPhysicsReg()) {
+            phyRegs.add(right);
+        }
+        return phyRegs;
+    }
+}
