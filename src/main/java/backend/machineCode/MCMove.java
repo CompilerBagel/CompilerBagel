9c8818c72372664a2669da2d5a02d231e830b47b
cardigan1008 <211250058@smail.nju.edu.cn>
2023-07-13 16:39:59 +0800

    feat: move

---
+++ src/main/java/backend/machineCode/MCMove.java
@@ -0,0 +1,52 @@
+package backend.machineCode;
+
+import java.util.ArrayList;
+
+public class MCMove extends MachineCode {
+    private MachineOperand src;
+    private MachineOperand dest;
+    
+    /**
+     * mv rd, rs1
+     * exec: reg[rd] <= reg[rs1] + 0
+     * @return mv rd, rs1
+     */
+    @Override
+    public String toString() {
+        // TODO: mv conditions
+        return "mv " + dest.toString() + ", " + src.toString();
+    }
+    
+    @Override
+    public void initUseAndDef() {
+        super.initUseAndDef();
+        use.add(src);
+        def.add(dest);
+    }
+    
+    @Override
+    public void replaceDef(final MachineOperand oldOperand, final MachineOperand newOperand) {
+        if (oldOperand.equals(this.src)) {
+            this.src = newOperand;
+        }
+    }
+    
+    @Override
+    public void replaceUse(final MachineOperand oldOperand, final MachineOperand newOperand) {
+        if (oldOperand.equals(this.dest)) {
+            this.dest = newOperand;
+        }
+    }
+    
+    @Override
+    public ArrayList<MachineOperand> allocatePhyRegs() {
+        ArrayList<MachineOperand> phyRegs = new ArrayList<>();
+        if (this.src.isPhysicsReg()) {
+            phyRegs.add(this.src);
+        }
+        if (this.dest.isPhysicsReg()) {
+            phyRegs.add(this.dest);
+        }
+        return phyRegs;
+    }
+}
