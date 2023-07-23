9754a611b71c00e76b58853574a78a9053132cf2
Nboxff <211250137@smail.nju.edu.cn>
2023-07-23 22:33:42 +0800

    test: fix wrong lib in gcc

--- functional_test.sh
+++ functional_test.sh
@@ -26,7 +26,7 @@ do
     echo $? >> answer.txt
 
     java -Dfile.encoding=UTF-8 -classpath ./target/classes:./lib/antlr4-runtime-4.9.1.jar Main ${sysy_filename} out_without_sylib.ll test.s
-    gcc test.s ./src/test/resources/libsysy.a -o out
+    gcc test.s ./src/test/resources/sylib.c -o out
     if [ -f $input_file ]; then
         timeout 60s ./out < $input_file > output.txt
     else
