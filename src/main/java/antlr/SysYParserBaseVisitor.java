7423a802d42567056571d9615293f428bbb08c21
cardigan1008 <211250058@smail.nju.edu.cn>
2023-07-31 15:57:03 +0800

    merge

--- src/main/java/antlr/SysYParserBaseVisitor.java
+++ src/main/java/antlr/SysYParserBaseVisitor.java
@@ -1,4 +1,4 @@
-package antlr;// Generated from ./src/main/java/antlr/SysYParser.g4 by ANTLR 4.9.1
+// Generated from ./src/main/java/antlr/SysYParser.g4 by ANTLR 4.12.0
 import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
 
 /**
@@ -9,6 +9,7 @@ import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
  * @param <T> The return type of the visit operation. Use {@link Void} for
  * operations with no return type.
  */
+@SuppressWarnings("CheckReturnValue")
 public class SysYParserBaseVisitor<T> extends AbstractParseTreeVisitor<T> implements SysYParserVisitor<T> {
 	/**
 	 * {@inheritDoc}
