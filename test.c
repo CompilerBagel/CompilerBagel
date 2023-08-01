f0a242d008a5b6c8f4bd38364878ba0c9896213b
cardigan1008 <211250058@smail.nju.edu.cn>
2023-08-01 10:53:15 +0800

    fix: cond with imm

--- test.c
+++ test.c
@@ -1,17 +1,25 @@
-int ififElse() {
-  int a;
-  a = 5;
-  int b;
-  b = 10;
-  if(a == 5)
-    if (b == 10)
-      a = 25;
-    else
-      a = a + 15;
-
-  return (a);
+int deepWhileBr(int a, int b) {
+  int c;
+  c = a + b;
+  while (c < 75) {
+    int d;
+    d = 42;
+    if (c < 100) {
+      c = c + d;
+      if (c > 99) {
+        int e;
+        e = d * 2;
+        if (1 == 1) {
+          c = e * 2;
+        }
+      }
+    }
+  }
+  return (c);
 }
 
-int main(){
-  return (ififElse());
+int main() {
+  int p;
+  p = 2;
+  return deepWhileBr(p, p);
 }
--- test.s
+++ test.s
@@ -1,65 +1,105 @@
 .global main
 .data
 .text
-ififElse:
-    addi sp, sp, -32
-    sd ra, 24(sp)
-    sd s0, 16(sp)
-    addi s0, sp, 32
-ififElseEntry1:
+deepWhileBr:
+    addi sp, sp, -48
+    sd ra, 40(sp)
+    sd s0, 32(sp)
+    addi s0, sp, 48
+deepWhileBrEntry1:
+    sw a0, -28(s0)
+    sw a0, -32(s0)
     li a0, 0
-    sw a0, -20(s0)
-    li a0, 5
-    sw a0, -20(s0)
-    li a0, 0
-    sw a0, -24(s0)
-    li a0, 10
-    sw a0, -24(s0)
-    lw a0, -20(s0)
-    li a1, 5
-    subw a1, a0, 5
-    seqz a0, a1
-    bne a0, zero, trueBlock2
-    j	falseBlock3
-trueBlock2:
-    lw a0, -24(s0)
-    li a1, 10
-    subw a1, a0, 10
-    seqz a0, a1
-    bne a0, zero, trueBlock5
-    j	falseBlock6
-falseBlock3:
+    sw a0, -36(s0)
+    lw a0, -28(s0)
+    lw a1, -32(s0)
+    addw a2, a0, a1
+    sw a2, -36(s0)
+    j	condBlock2
+condBlock2:
+    lw a0, -36(s0)
+    li a1, 75
+    slt a2, a0, a1
+    bne a2, zero, bodyBlock3
     j	afterBlock4
+bodyBlock3:
+    li a0, 0
+    sw a0, -40(s0)
+    li a0, 42
+    sw a0, -40(s0)
+    lw a0, -36(s0)
+    li a1, 100
+    slt a2, a0, a1
+    bne a2, zero, trueBlock5
+    j	falseBlock6
 afterBlock4:
-    lw a0, -20(s0)
+    lw a0, -36(s0)
     addw a0, a0, 0
-    ld ra, 24(sp)
-    ld s0, 16(sp)
-    addi sp, sp, 32
+    ld ra, 40(sp)
+    ld s0, 32(sp)
+    addi sp, sp, 48
     ret
     li a0, 0
-    ld ra, 24(sp)
-    ld s0, 16(sp)
-    addi sp, sp, 32
+    ld ra, 40(sp)
+    ld s0, 32(sp)
+    addi sp, sp, 48
     ret
 trueBlock5:
-    li a0, 25
-    sw a0, -20(s0)
-    j	afterBlock7
+    lw a0, -36(s0)
+    lw a1, -40(s0)
+    addw a2, a0, a1
+    sw a2, -36(s0)
+    lw a0, -36(s0)
+    li a1, 99
+    slt a2, a1, a0
+    bne a2, zero, trueBlock8
+    j	falseBlock9
 falseBlock6:
-    lw a0, -20(s0)
-    addiw a1, a0, 15
-    sw a1, -20(s0)
     j	afterBlock7
 afterBlock7:
-    j	afterBlock4
+    j	condBlock2
+trueBlock8:
+    li a0, 0
+    sw a0, -44(s0)
+    lw a0, -40(s0)
+    li a1, 2
+    mulw a1, a0, a1
+    sw a1, -44(s0)
+    li a0, 1
+    li a1, 1
+    subw a2, a0, a1
+    seqz a0, a2
+    bne a0, zero, trueBlock11
+    j	falseBlock12
+falseBlock9:
+    j	afterBlock10
+afterBlock10:
+    j	afterBlock7
+trueBlock11:
+    lw a0, -44(s0)
+    li a1, 2
+    mulw a1, a0, a1
+    sw a1, -36(s0)
+    j	afterBlock13
+falseBlock12:
+    j	afterBlock13
+afterBlock13:
+    j	afterBlock10
 main:
     addi sp, sp, -32
     sd ra, 24(sp)
     sd s0, 16(sp)
     addi s0, sp, 32
-mainEntry8:
-    call ififElse
+mainEntry14:
+    li a0, 0
+    sw a0, -20(s0)
+    li a0, 2
+    sw a0, -20(s0)
+    lw a0, -20(s0)
+    lw a0, -20(s0)
+    lw a0, 0(a0)
+    lw a0, 0(a0)
+    call deepWhileBr
     addw a0, a0, 0
     ld ra, 24(sp)
     ld s0, 16(sp)
