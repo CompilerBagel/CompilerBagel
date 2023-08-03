c05fd537b2a0d0943b8ecbad92546ebad00b0842
Yunbo Ni <211250058@smail.nju.edu.cn>
2023-08-03 22:47:48 +0800

    merge

--- test.s
+++ test.s
@@ -1,77 +1,290 @@
 .global main
 .data
 .text
-main:
-mainEntry1:
-    addi sp, sp, -480
-    sd ra, 472(sp)
-    sd s0, 464(sp)
-    addi s0, sp, 480
+foo:
+fooEntry1:
+    addi sp, sp, -160
+    sd ra, 152(sp)
+    sd s0, 144(sp)
+    addi s0, sp, 160
+    addi a1, s0, -24
     li a1, 0
-    sw a1, -424(s0)
+    sw a1, -24(s0)
+    addi a1, s0, -28
+    li a1, 1
+    sw a1, -28(s0)
+    addi a1, s0, -32
+    li a1, 2
+    sw a1, -32(s0)
+    addi a1, s0, -36
+    li a1, 3
+    sw a1, -36(s0)
+    addi a1, s0, -40
     li a1, 0
-    sw a1, -428(s0)
-    j	condBlock2
-condBlock2:
-    sw a1, -428(s0)
-    sw a2, -436(s0)
-    sw a3, -444(s0)
-    call getint
-    lw a1, -428(s0)
-    lw a2, -436(s0)
-    lw a3, -444(s0)
+    sw a1, -40(s0)
+    addi a1, s0, -44
+    li a1, 1
+    sw a1, -44(s0)
+    addi a1, s0, -48
+    li a1, 2
+    sw a1, -48(s0)
+    addi a1, s0, -52
+    li a1, 3
+    sw a1, -52(s0)
+    addi a1, s0, -56
     li a1, 0
-    subw a1, a0, a1
-    snez a1, a1
-    bne a1, zero, bodyBlock3
-    j	afterBlock4
-bodyBlock3:
-    lw a1, -424(s0)
-    li a2, 4
-    mulw a1, a1, a2
-    addi a1, a1, 24
-    sub a1, s0, a1
-    sw a1, -452(s0)
-    sw a2, -460(s0)
-    sw a3, -468(s0)
-    call getint
-    lw a1, -452(s0)
-    lw a2, -460(s0)
-    lw a3, -468(s0)
-    sw a0, 0(a1)
-    lw a1, -424(s0)
-    addiw a1, a1, 1
-    sw a1, -424(s0)
-    j	condBlock2
-afterBlock4:
-    j	condBlock5
-condBlock5:
-    lw a1, -424(s0)
-    li a2, 0
-    subw a1, a1, a2
-    snez a1, a1
-    bne a1, zero, bodyBlock6
-    j	afterBlock7
-bodyBlock6:
-    lw a1, -424(s0)
-    addiw a1, a1, -1
-    sw a1, -424(s0)
-    lw a1, -428(s0)
-    lw a2, -424(s0)
+    sw a1, -56(s0)
+    addi a1, s0, -60
+    li a1, 1
+    sw a1, -60(s0)
+    addi a1, s0, -64
+    li a1, 2
+    sw a1, -64(s0)
+    addi a1, s0, -68
+    li a1, 3
+    sw a1, -68(s0)
+    addi a1, s0, -72
+    li a1, 0
+    sw a1, -72(s0)
+    addi a1, s0, -76
+    li a1, 1
+    sw a1, -76(s0)
+    addi a1, s0, -80
+    li a1, 2
+    sw a1, -80(s0)
+    addi a1, s0, -84
+    li a1, 3
+    sw a1, -84(s0)
+    li a1, 3
+    sw a1, -88(s0)
+    li a1, 7
+    sw a1, -92(s0)
+    li a1, 5
+    sw a1, -96(s0)
+    li a1, 6
+    sw a1, -100(s0)
+    li a1, 1
+    sw a1, -104(s0)
+    li a1, 0
+    sw a1, -108(s0)
+    li a1, 3
+    sw a1, -112(s0)
+    li a1, 5
+    sw a1, -116(s0)
+    li a1, 4
+    sw a1, -120(s0)
+    li a1, 2
+    sw a1, -124(s0)
+    li a1, 7
+    sw a1, -128(s0)
+    li a1, 9
+    sw a1, -132(s0)
+    li a1, 8
+    sw a1, -136(s0)
+    li a1, 1
+    sw a1, -140(s0)
+    li a1, 4
+    sw a1, -144(s0)
+    li a1, 6
+    sw a1, -148(s0)
+    lw a1, -88(s0)
+    lw a2, -92(s0)
+    addw a1, a1, a2
+    lw a2, -96(s0)
+    addw a1, a1, a2
+    lw a2, -100(s0)
+    addw a1, a1, a2
+    lw a2, -104(s0)
+    addw a1, a1, a2
+    lw a2, -108(s0)
+    addw a1, a1, a2
+    lw a2, -112(s0)
+    addw a1, a1, a2
+    lw a2, -116(s0)
+    addw a1, a1, a2
+    sw a1, -152(s0)
+    lw a1, -120(s0)
+    lw a2, -124(s0)
+    addw a1, a1, a2
+    lw a2, -128(s0)
+    addw a1, a1, a2
+    lw a2, -132(s0)
+    addw a1, a1, a2
+    lw a2, -136(s0)
+    addw a1, a1, a2
+    lw a2, -140(s0)
+    addw a1, a1, a2
+    lw a2, -144(s0)
+    addw a1, a1, a2
+    lw a2, -148(s0)
+    addw a1, a1, a2
+    sw a1, -156(s0)
+    lw a1, -152(s0)
+    lw a2, -156(s0)
+    addw a1, a1, a2
+    lw a2, -88(s0)
     li a3, 4
     mulw a2, a2, a3
     addi a2, a2, 24
     sub a2, s0, a2
     lw a2, 0(a2)
     addw a1, a1, a2
-    sw a1, -428(s0)
-    j	condBlock5
-afterBlock7:
-    lw a1, -428(s0)
     addi a0, a1, 0
-    ld ra, 472(sp)
-    ld s0, 464(sp)
-    addi sp, sp, 480
+    ld ra, 152(sp)
+    ld s0, 144(sp)
+    addi sp, sp, 160
+    ret
+    li a0, 0
+    ret
+main:
+mainEntry2:
+    addi sp, sp, -192
+    sd ra, 184(sp)
+    sd s0, 176(sp)
+    addi s0, sp, 192
+    li a1, 3
+    sw a1, -24(s0)
+    li a1, 7
+    sw a1, -28(s0)
+    li a1, 5
+    sw a1, -32(s0)
+    li a1, 6
+    sw a1, -36(s0)
+    li a1, 1
+    sw a1, -40(s0)
+    li a1, 0
+    sw a1, -44(s0)
+    li a1, 3
+    sw a1, -48(s0)
+    li a1, 5
+    sw a1, -52(s0)
+    li a1, 4
+    sw a1, -56(s0)
+    li a1, 2
+    sw a1, -60(s0)
+    li a1, 7
+    sw a1, -64(s0)
+    li a1, 9
+    sw a1, -68(s0)
+    li a1, 8
+    sw a1, -72(s0)
+    li a1, 1
+    sw a1, -76(s0)
+    li a1, 4
+    sw a1, -80(s0)
+    li a1, 6
+    sw a1, -84(s0)
+    lw a1, -24(s0)
+    lw a2, -28(s0)
+    addw a1, a1, a2
+    lw a2, -32(s0)
+    addw a1, a1, a2
+    lw a2, -36(s0)
+    addw a1, a1, a2
+    lw a2, -40(s0)
+    addw a1, a1, a2
+    lw a2, -44(s0)
+    addw a1, a1, a2
+    lw a2, -48(s0)
+    addw a1, a1, a2
+    lw a2, -52(s0)
+    addw a1, a1, a2
+    sw a1, -88(s0)
+    lw a1, -56(s0)
+    lw a2, -60(s0)
+    addw a1, a1, a2
+    lw a2, -64(s0)
+    addw a1, a1, a2
+    lw a2, -68(s0)
+    addw a1, a1, a2
+    lw a2, -72(s0)
+    addw a1, a1, a2
+    lw a2, -76(s0)
+    addw a1, a1, a2
+    lw a2, -80(s0)
+    addw a1, a1, a2
+    lw a2, -84(s0)
+    addw a1, a1, a2
+    sw a1, -92(s0)
+    lw a1, -88(s0)
+    sw a1, -92(s0)
+    sw a2, -100(s0)
+    sw a3, -108(s0)
+    call foo
+    lw a1, -92(s0)
+    lw a2, -100(s0)
+    lw a3, -108(s0)
+    addw a1, a1, a0
+    sw a1, -88(s0)
+    li a1, 4
+    sw a1, -120(s0)
+    li a1, 7
+    sw a1, -124(s0)
+    li a1, 2
+    sw a1, -128(s0)
+    li a1, 5
+    sw a1, -132(s0)
+    li a1, 8
+    sw a1, -136(s0)
+    li a1, 0
+    sw a1, -140(s0)
+    li a1, 6
+    sw a1, -144(s0)
+    li a1, 3
+    sw a1, -148(s0)
+    lw a1, -92(s0)
+    sw a1, -148(s0)
+    sw a2, -156(s0)
+    sw a3, -164(s0)
+    call foo
+    lw a1, -148(s0)
+    lw a2, -156(s0)
+    lw a3, -164(s0)
+    addw a1, a1, a0
+    sw a1, -92(s0)
+    lw a1, -56(s0)
+    sw a1, -24(s0)
+    lw a1, -60(s0)
+    sw a1, -28(s0)
+    lw a1, -64(s0)
+    sw a1, -32(s0)
+    lw a1, -68(s0)
+    sw a1, -36(s0)
+    lw a1, -72(s0)
+    sw a1, -40(s0)
+    lw a1, -76(s0)
+    sw a1, -44(s0)
+    lw a1, -80(s0)
+    sw a1, -48(s0)
+    lw a1, -84(s0)
+    sw a1, -52(s0)
+    lw a1, -120(s0)
+    lw a2, -124(s0)
+    addw a1, a1, a2
+    lw a2, -128(s0)
+    addw a1, a1, a2
+    lw a2, -132(s0)
+    addw a1, a1, a2
+    lw a2, -136(s0)
+    addw a1, a1, a2
+    lw a2, -140(s0)
+    addw a1, a1, a2
+    lw a2, -144(s0)
+    addw a1, a1, a2
+    lw a2, -148(s0)
+    addw a1, a1, a2
+    sw a1, -176(s0)
+    lw a1, -88(s0)
+    lw a2, -92(s0)
+    addw a1, a1, a2
+    lw a2, -176(s0)
+    addw a1, a1, a2
+    sw a1, -180(s0)
+    lw a1, -180(s0)
+    addi a0, a1, 0
+    ld ra, 184(sp)
+    ld s0, 176(sp)
+    addi sp, sp, 192
     ret
     li a0, 0
     ret
