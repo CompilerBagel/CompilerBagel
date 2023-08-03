c50feb9f4c21009edfb5172f9f9080868c65d537
Yunbo Ni <211250058@smail.nju.edu.cn>
2023-08-03 22:30:24 +0800

    fix: miss when merge

--- test.c
+++ test.c
@@ -1,13 +1,22 @@
+// ???  // ????
+  				// ?????
+/*
+
 int main() {
-  int arr[2], sum = 0;
-  int count = 0;
-  while (count != 2) {
-  	arr[count] = count;
-    count = count + 1;
+  int arr[100], i = 0, sum = 0;
+  while (getint()) {
+  	arr[i] = getint();
+    i = i + 1;
+  }*/
+int main() {
+  int arr[100], i = 0, sum = 0;
+  while (getint()) {
+  	arr[i] = getint();
+    i = i + 1;
   }
-  while (count != 2) {
-    count = count - 1;
-    sum = sum + arr[count];
+	while (i) {
+    i = i - 1;
+    sum = sum + arr[i];
   }
   return sum;
 }
