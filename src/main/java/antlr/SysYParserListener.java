50ef8bceedaf4827c4f76b3faaeb464af46999e9
Nboxff <211250137@smail.nju.edu.cn>
2023-08-04 09:29:42 +0800

    test: test the call

--- src/main/java/antlr/SysYParserListener.java
+++ src/main/java/antlr/SysYParserListener.java
@@ -1,4 +1,4 @@
-// Generated from ./src/main/java/antlr/SysYParser.g4 by ANTLR 4.12.0
+package antlr;// Generated from ./src/main/java/antlr/SysYParser.g4 by ANTLR 4.12.0
 import org.antlr.v4.runtime.tree.ParseTreeListener;
 
 /**
