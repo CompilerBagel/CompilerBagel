c6fca66f7631d9e29fb7ee7f1a0ab5b3dbd11aaf
huangwei021230 <1138608506@qq.com>
2023-08-07 21:47:01 +0800

    fix: delete irrelevant files commit records 2nd

--- src/main/java/IRGenVisitor.java
+++ src/main/java/IRGenVisitor.java
@@ -543,7 +543,6 @@ public class IRGenVisitor extends SysYParserBaseVisitor<ValueRef> {
                 }
             }
 
-
             args.add(i, param);
         }
         return IRBuildCall(builder, functionBlock, args, argc, funcName);
