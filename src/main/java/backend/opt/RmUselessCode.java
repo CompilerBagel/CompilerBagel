7d389076f40305f8ddd92fff5e36efd41563387e
Nboxff <211250137@smail.nju.edu.cn>
2023-08-16 22:13:30 +0800

    feat: remove useless store like in code like

--- src/main/java/backend/opt/RmUselessCode.java
+++ src/main/java/backend/opt/RmUselessCode.java
@@ -1,28 +1,70 @@
 package backend.opt;
 
 import backend.machineCode.*;
+import backend.machineCode.Instruction.MCLi;
 import backend.machineCode.Instruction.MCLoad;
 import backend.machineCode.Instruction.MCReturn;
 import backend.machineCode.Instruction.MCStore;
 import backend.post.reg.PhysicsReg;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+
+import static backend.machineCode.MachineConstants.LW;
+import static backend.machineCode.MachineConstants.SW;
 
 public class RmUselessCode {
     private final List<MachineFunction> functions;
+
     public RmUselessCode(List<MachineFunction> functions) {
         this.functions = functions;
     }
 
+
+    static class StoreQueue {
+        List<MachineCode> rmList;
+        MachineCode reserveCode;
+
+        StoreQueue() {
+            rmList = new ArrayList<>();
+            reserveCode = null;
+        }
+
+        void push(MachineCode nowCode) {
+            if (reserveCode != null) {
+                rmList.add(reserveCode);
+            }
+            reserveCode = nowCode;
+        }
+
+        void clear() {
+            rmList.clear();
+            reserveCode = null;
+        }
+
+        List<MachineCode> getRmList() {
+            return rmList;
+        }
+    }
+
+    private HashMap<Integer, StoreQueue> addrStoreCodes;
+
+    /**
+     * remove useless codes in all functions
+     */
     public void remove() {
         for (MachineFunction function : functions) {
             funcRemove(function);
         }
     }
+
+    /**
+     * remove useless in function
+     *
+     * @param function the MachineFunction to be dealt
+     */
     public void funcRemove(MachineFunction function) {
+        funcRmUselessStore(function);
+
         LinkedList<MachineBlock> blocks = function.getMachineBlocks();
 
         for (MachineBlock block : blocks) {
@@ -34,21 +76,65 @@ public class RmUselessCode {
             List<MachineCode> removeList = new ArrayList<>();
             Iterator<MachineCode> iterator = codes.iterator();
             MachineCode lastCode = null;
-            while(iterator.hasNext()) {
+            while (iterator.hasNext()) {
                 MachineCode code = iterator.next();
-                if(lastCode instanceof MCReturn){
+                if (lastCode instanceof MCReturn) {
                     removeList.add(code);
                     continue;
-                } else if (isRedundancyLS(lastCode, code)){
+                } else if (isRedundancyLS(lastCode, code)) {
                     // 1. LD r0, a  2. ST a, R0
                     removeList.add(code);
                     continue;
+                } else if (lastCode instanceof MCLi && code instanceof MCLi) {
+                    PhysicsReg lastCodeReg = getReg(((MCLi) lastCode).getDest());
+                    PhysicsReg codeReg = getReg(((MCLi) code).getDest());
+                    if (lastCodeReg == codeReg) {
+                        removeList.add(lastCode);
+                    }
                 }
                 lastCode = code;
             }
             codes.removeAll(removeList);
         }
+    }
 
+    private void funcRmUselessStore(MachineFunction function) {
+        LinkedList<MachineBlock> blocks = function.getMachineBlocks();
+
+        for (MachineBlock block : blocks) {
+            addrStoreCodes = new HashMap<>();
+            List<MachineCode> codes = block.getMachineCodes();
+            List<MachineCode> removeList = new ArrayList<>();
+            for (MachineCode code : codes) {
+                // SW in stack space
+                if (code instanceof MCStore
+                        && getReg(((MCStore) code).getDest()) == PhysicsReg.getS0Reg()
+                        && ((MCStore) code).getStoreOp().equals(SW)) {
+                    int address = ((Immeidiate) ((MCStore) code).getOffset()).getImmValue();
+                    StoreQueue storeQueue = addrStoreCodes.get(address);
+                    if (storeQueue == null) {
+                        storeQueue = new StoreQueue();
+                        addrStoreCodes.put(address, storeQueue);
+                    }
+                    storeQueue.push(code);
+                } else if (code instanceof MCLoad
+                        && getReg(((MCLoad) code).getSrc()) == PhysicsReg.getS0Reg()
+                        && ((MCLoad) code).getLoadOp().equals(LW)) {
+                    int address = ((Immeidiate) ((MCLoad) code).getOffset()).getImmValue();
+                    StoreQueue storeQueue = addrStoreCodes.get(address);
+                    if (storeQueue == null) continue;
+                    removeList.addAll(storeQueue.getRmList());
+                    storeQueue.clear();
+                }
+            }
+            codes.removeAll(removeList);
+            for (StoreQueue storeQueue : addrStoreCodes.values()) {
+                if (storeQueue != null && storeQueue.rmList.size() != 0) {
+                    codes.removeAll(storeQueue.rmList);
+                    storeQueue.rmList.clear();
+                }
+            }
+        }
     }
 
     private boolean isRedundancyLS(MachineCode loadCode, MachineCode storeCode) {
@@ -68,6 +154,12 @@ public class RmUselessCode {
                 && loadImm.getImmValue() == storeImm.getImmValue();
     }
 
+    /**
+     * Get the PhysicsReg allocated to the MachineOperand
+     *
+     * @param op MachineOperand Object
+     * @return allocated PhysicsReg
+     */
     private PhysicsReg getReg(MachineOperand op) {
         if (op instanceof PhysicsReg) {
             return (PhysicsReg) op;
