fad8b220aca027db58df5bdf550d72532daaec0a
Yunbo Ni <211250058@smail.nju.edu.cn>
2023-08-19 12:58:19 +0800

    feat: postProcessor singleton

--- src/main/java/backend/post/PostProcessor.java
+++ src/main/java/backend/post/PostProcessor.java
@@ -1,4 +1,14 @@
 package backend.post;
 
+import java.util.Objects;
+
 public class PostProcessor {
+
+    private static PostProcessor instance = null;
+
+    private PostProcessor(){};
+
+    public static PostProcessor getInstance() {
+        return Objects.requireNonNullElseGet(instance, PostProcessor::new);
+    }
 }
